00c16d0ad8f9c471457b2d0e2d343f6fd8b70ea1
Yaroslav Omelianenko <Omelianenkoyarosllav@.com>
2020-06-27 19:08:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simplehoney2
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+﻿const gulp = require('gulp');
+const browserSync = require('browser-sync');
+
+
+// Static server
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "C:/Users/Yaroslav/Desktop/trepa40v/practic16/4"
+        }
+    });
+
+    gulp.watch('C:/Users/Yaroslav/Desktop/trepa40v/practic16/4/**/*').on('change', browserSync.reload);
+});
+
+
+
